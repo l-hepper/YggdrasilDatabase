54f384b198973d7bee2463bebe1ad8d3854cb841
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-10 16:24:45 +0100

    feat: Add service constructor

--- Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
@@ -1,7 +1,22 @@
 package com.mjolnir.yggdrasil.service;
 
+import com.mjolnir.yggdrasil.repositories.CityRepository;
+import com.mjolnir.yggdrasil.repositories.CountryLanguageIdRepository;
+import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
+import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorldService {
+    private CityRepository cityRepository;
+    private CountryLanguageIdRepository countryLanguageIdRepository;
+    private CountryLanguageRepository countryLanguageRepository;
+    private CountryRepository countryRepository;
+
+    public WorldService(CityRepository cityRepository, CountryLanguageIdRepository countryLanguageIdRepository, CountryLanguageRepository countryLanguageRepository, CountryRepository countryRepository) {
+        this.cityRepository = cityRepository;
+        this.countryLanguageIdRepository = countryLanguageIdRepository;
+        this.countryLanguageRepository = countryLanguageRepository;
+        this.countryRepository = countryRepository;
+    }
 }
