86b398e0bb2c5c5957d4f94b70283d0d92d57daa
Devin Colyer <dcolyer@spartaglobal.com>
2024-07-10 17:02:38 +0100

    Update methods added to WorldService Class

--- Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
@@ -1,11 +1,14 @@
 package com.mjolnir.yggdrasil.service;
 
+import com.mjolnir.yggdrasil.entities.CityEntity;
 import com.mjolnir.yggdrasil.repositories.CityRepository;
 import com.mjolnir.yggdrasil.repositories.CountryLanguageIdRepository;
 import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
 import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class WorldService {
@@ -21,4 +24,47 @@ public class WorldService {
         this.countryLanguageRepository = countryLanguageRepository;
         this.countryRepository = countryRepository;
     }
+
+    public boolean updateCityById(Integer id, CityEntity city){
+        Optional<CityEntity> cityToAlter = cityRepository.findById(id);
+        if(cityToAlter.isPresent() && city !=  null){
+            cityToAlter.get().setName(city.getName());
+            cityToAlter.get().setDistrict(city.getDistrict());
+            cityToAlter.get().setPopulation(city.getPopulation());
+            cityRepository.save(cityToAlter.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateCityNameById(Integer id, String cityName){
+        Optional<CityEntity> cityToAlter = cityRepository.findById(id);
+        if(cityToAlter.isPresent() && cityName !=  null){
+            cityToAlter.get().setName(cityName);
+            cityRepository.save(cityToAlter.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateCityDistrictById(Integer id, String cityDistrict){
+        Optional<CityEntity> cityToAlter = cityRepository.findById(id);
+        if(cityToAlter.isPresent() && cityDistrict !=  null){
+            cityToAlter.get().setDistrict(cityDistrict);
+            cityRepository.save(cityToAlter.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateCityPopulationById(Integer id, Integer population){
+        Optional<CityEntity> cityToAlter = cityRepository.findById(id);
+        if(cityToAlter.isPresent() && population !=  null){
+            cityToAlter.get().setPopulation(population);
+            cityRepository.save(cityToAlter.get());
+            return true;
+        }
+        return false;
+    }
+
 }
