a90df5fd7b34e37ba9936654de34ba9db0f6ae01
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-10 18:34:10 +0100

    feat: Build createNewCity method

--- Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
@@ -1,24 +1,40 @@
 package com.mjolnir.yggdrasil.service;
 
+import com.mjolnir.yggdrasil.entities.CityEntity;
+import com.mjolnir.yggdrasil.entities.CountryEntity;
 import com.mjolnir.yggdrasil.repositories.CityRepository;
-import com.mjolnir.yggdrasil.repositories.CountryLanguageIdRepository;
 import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
 import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class WorldService {
     private CityRepository cityRepository;
-    private CountryLanguageIdRepository countryLanguageIdRepository;
     private CountryLanguageRepository countryLanguageRepository;
     private CountryRepository countryRepository;
 
     @Autowired
-    public WorldService(CityRepository cityRepository, CountryLanguageIdRepository countryLanguageIdRepository, CountryLanguageRepository countryLanguageRepository, CountryRepository countryRepository) {
+    public WorldService(CityRepository cityRepository, CountryLanguageRepository countryLanguageRepository, CountryRepository countryRepository) {
         this.cityRepository = cityRepository;
-        this.countryLanguageIdRepository = countryLanguageIdRepository;
         this.countryLanguageRepository = countryLanguageRepository;
         this.countryRepository = countryRepository;
+    }
+
+
+    public boolean createNewCity(String countryCode, String cityName, String cityDistrict, int population) {
+        Optional<CountryEntity> countryEntity = countryRepository.findById(countryCode);
+        if (countryEntity.isPresent() && cityName != null && cityDistrict != null && population > 0) {
+            CityEntity cityEntity = new CityEntity();
+            cityEntity.setName(cityName);
+            cityEntity.setCountryCode(countryEntity.get());
+            cityEntity.setDistrict(cityDistrict);
+            cityEntity.setPopulation(population);
+            cityRepository.save(cityEntity);
+            return true;
+        }
+        return false;
     }
 }
