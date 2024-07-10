a7388ea35bf7e18e76661fb85ff07c4af88a7346
l-hepper <liamhepper1@gmail.com>
2024-07-10 19:39:38 +0100

    added deleteLanguageByLanguageMethod() and unit tests

--- Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/service/WorldService.java
@@ -2,28 +2,34 @@ package com.mjolnir.yggdrasil.service;
 
 import com.mjolnir.yggdrasil.entities.CityEntity;
 import com.mjolnir.yggdrasil.entities.CountryEntity;
+import com.mjolnir.yggdrasil.entities.CountryLanguageEntity;
+import com.mjolnir.yggdrasil.entities.CountryLanguageIdEntity;
 import com.mjolnir.yggdrasil.repositories.CityRepository;
 //import com.mjolnir.yggdrasil.repositories.CountryLanguageIdRepository;
 import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
 import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class WorldService {
+    private final DataSourceTransactionManagerAutoConfiguration dataSourceTransactionManagerAutoConfiguration;
     private CityRepository cityRepository;
 //    private CountryLanguageIdRepository countryLanguageIdRepository;
     private CountryLanguageRepository countryLanguageRepository;
     private CountryRepository countryRepository;
 
     @Autowired
-    public WorldService(CityRepository cityRepository, CountryLanguageRepository countryLanguageRepository, CountryRepository countryRepository) {
+    public WorldService(CityRepository cityRepository, CountryLanguageRepository countryLanguageRepository, CountryRepository countryRepository, DataSourceTransactionManagerAutoConfiguration dataSourceTransactionManagerAutoConfiguration) {
         this.cityRepository = cityRepository;
 //        this.countryLanguageIdRepository = countryLanguageIdRepository;
         this.countryLanguageRepository = countryLanguageRepository;
         this.countryRepository = countryRepository;
+        this.dataSourceTransactionManagerAutoConfiguration = dataSourceTransactionManagerAutoConfiguration;
     }
 
     public boolean deleteCountryByCode(String countryCode) {
@@ -48,5 +54,34 @@ public class WorldService {
         }
     }
 
+    public boolean deleteLanguageByCountryCodeAndLanguage(String countryCode, String language) {
+        CountryLanguageIdEntity primaryKey = new CountryLanguageIdEntity();
+        primaryKey.setCountryCode(countryCode);
+        primaryKey.setLanguage(language);
 
+        Optional<CountryLanguageEntity> countryLanguage = countryLanguageRepository.findById(primaryKey);
+        if (countryLanguage.isPresent()) {
+            countryLanguageRepository.delete(countryLanguage.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteLanguageByLanguage(String languageToDelete) {
+        List<CountryLanguageEntity> countryLanguages = countryLanguageRepository.findAll();
+
+        for (CountryLanguageEntity language : countryLanguages) {
+            if (language.getId().getLanguage().equals(languageToDelete)) {
+                countryLanguageRepository.delete(language);
+            }
+        }
+
+        List<CountryLanguageEntity> newCountryLanguages = countryLanguageRepository.findAll();
+        if (newCountryLanguages.size() < countryLanguages.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- Yggdrasil/src/test/java/com/mjolnir/yggdrasil/DeletionMethodsTests.java
+++ Yggdrasil/src/test/java/com/mjolnir/yggdrasil/DeletionMethodsTests.java
@@ -1,7 +1,10 @@
 package com.mjolnir.yggdrasil;
 
 import com.mjolnir.yggdrasil.entities.CityEntity;
+import com.mjolnir.yggdrasil.entities.CountryLanguageEntity;
+import com.mjolnir.yggdrasil.entities.CountryLanguageIdEntity;
 import com.mjolnir.yggdrasil.repositories.CityRepository;
+import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
 import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import com.mjolnir.yggdrasil.service.WorldService;
 import jakarta.transaction.Transactional;
@@ -25,6 +28,8 @@ public class DeletionMethodsTests {
 
     @Autowired
     private CityRepository cityRepository;
+    @Autowired
+    private CountryLanguageRepository countryLanguageRepository;
 
     @Test
     void testDeleteCountryByCode() {
@@ -60,6 +65,55 @@ public class DeletionMethodsTests {
     }
 
     @Test
+    void testAllForeignKeyRecordsInCountryLanguageTableAreDeleted() {
+        CountryLanguageIdEntity arabic = new CountryLanguageIdEntity();
+        arabic.setCountryCode("AUS");
+        arabic.setLanguage("Arabic");
+
+        CountryLanguageIdEntity cantonChinese = new CountryLanguageIdEntity();
+        cantonChinese.setCountryCode("AUS");
+        cantonChinese.setLanguage("Canton Chinese");
+
+        CountryLanguageIdEntity english = new CountryLanguageIdEntity();
+        english.setCountryCode("AUS");
+        english.setLanguage("English");
+
+        CountryLanguageIdEntity german = new CountryLanguageIdEntity();
+        german.setCountryCode("AUS");
+        german.setLanguage("German");
+
+        CountryLanguageIdEntity greek = new CountryLanguageIdEntity();
+        greek.setCountryCode("AUS");
+        greek.setLanguage("Greek");
+
+        CountryLanguageIdEntity italian = new CountryLanguageIdEntity();
+        italian.setCountryCode("AUS");
+        italian.setLanguage("Italian");
+
+        CountryLanguageIdEntity serboCroatian = new CountryLanguageIdEntity();
+        serboCroatian.setCountryCode("AUS");
+        serboCroatian.setLanguage("Serbo-Croatian");
+
+        CountryLanguageIdEntity vietnamese = new CountryLanguageIdEntity();
+        vietnamese.setCountryCode("AUS");
+        vietnamese.setLanguage("Vietnamese");
+
+        worldService.deleteCountryByCode("AUS");
+        List<CountryLanguageEntity> list = countryLanguageRepository.findAllById(
+                new ArrayList(
+                        List.of(
+                                arabic,
+                                cantonChinese,
+                                english,
+                                german,
+                                greek,
+                                italian,
+                                serboCroatian,
+                                vietnamese)));
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @Test
     void testDeleteCityById() {
         worldService.deleteCityById(119);
         boolean exists = cityRepository.existsById(119);
@@ -79,4 +133,53 @@ public class DeletionMethodsTests {
         Assertions.assertTrue(isDeletionSuccessful);
     }
 
+    @Test
+    void testDeleteLanguageByCountryCodeAndLanguage() {
+        worldService.deleteLanguageByCountryCodeAndLanguage("AUS", "Arabic");
+        CountryLanguageIdEntity primaryKey = new CountryLanguageIdEntity();
+        primaryKey.setCountryCode("AUS");
+        primaryKey.setLanguage("Arabic");
+        boolean exists = countryLanguageRepository.existsById(primaryKey);
+        Assertions.assertFalse(exists);
+    }
+
+    @Test
+    void testDeleteLanguageByCountryCodeAndLanguageReturnsFalseForNonExistentLanguage() {
+        boolean isDeleted = worldService.deleteLanguageByCountryCodeAndLanguage("AUS", "Tongan");
+        Assertions.assertFalse(isDeleted);
+    }
+
+    @Test
+    void testDeleteLanguageByCountryCodeAndLanguageReturnsFalseForNonExistentCountry() {
+        boolean isDeleted = worldService.deleteLanguageByCountryCodeAndLanguage("XXX", "Tongan");
+        Assertions.assertFalse(isDeleted);
+    }
+
+    @Test
+    void testDeleteLanguageByLanguageDeletesAllRelevantLanguageRecords() {
+        worldService.deleteLanguageByLanguage("Tongan");
+
+        CountryLanguageIdEntity tonganInASM = new CountryLanguageIdEntity();
+        tonganInASM.setCountryCode("ASM");
+        tonganInASM.setLanguage("Tongan");
+
+        CountryLanguageIdEntity tonganInTON = new CountryLanguageIdEntity();
+        tonganInTON.setCountryCode("TON");
+        tonganInTON.setLanguage("Tongan");
+
+        boolean exists = countryLanguageRepository.existsById(tonganInASM);
+        Assertions.assertFalse(exists);
+    }
+
+    @Test
+    void testDeleteLanguageByLanguageReturnsFalseForNonExistentLanguage() {
+        boolean isDeleted = worldService.deleteLanguageByLanguage("Klingon");
+        Assertions.assertFalse(isDeleted);
+    }
+
+    @Test
+    void testDeleteLanguageByLanguageReturnsTrueForExistentLanguage() {
+        boolean isDeleted = worldService.deleteLanguageByLanguage("Arabic");
+        Assertions.assertTrue(isDeleted);
+    }
 }
