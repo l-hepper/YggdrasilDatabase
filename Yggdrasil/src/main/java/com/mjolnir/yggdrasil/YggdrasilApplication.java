e3fbea3e3ef366cfc5053ccc25b976a2a8ac7f86
Devin Colyer <dcolyer@spartaglobal.com>
2024-07-10 15:13:32 +0100

    lol

---
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/YggdrasilApplication.java
@@ -0,0 +1,13 @@
+package com.mjolnir.yggdrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YggdrasilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YggdrasilApplication.class, args);
+    }
+
+}
---
+++ Yggdrasil/src/test/java/com/mjolnir/yggdrasil/YggdrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjolnir.yggdrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YggdrasilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
