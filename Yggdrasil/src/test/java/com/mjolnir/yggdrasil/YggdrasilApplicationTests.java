c31a17bffef8cdb83996d889139093b09d75273c
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-11 11:55:30 +0100

    test: Build tests for createNewCountryLanguage and further tests for createNewCountry

--- Yggdrasil/src/test/java/com/mjolnir/yggdrasil/YggdrasilApplicationTests.java
+++ Yggdrasil/src/test/java/com/mjolnir/yggdrasil/YggdrasilApplicationTests.java
@@ -2,11 +2,14 @@ package com.mjolnir.yggdrasil;
 
 import com.mjolnir.yggdrasil.entities.CityEntity;
 import com.mjolnir.yggdrasil.entities.CountryEntity;
+import com.mjolnir.yggdrasil.entities.CountryLanguageEntity;
 import com.mjolnir.yggdrasil.repositories.CityRepository;
 import com.mjolnir.yggdrasil.repositories.CountryLanguageRepository;
 import com.mjolnir.yggdrasil.repositories.CountryRepository;
 import com.mjolnir.yggdrasil.service.WorldService;
 import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.AssertTrue;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -134,11 +137,207 @@ class YggdrasilApplicationTests {
         assert countryEntity != null;
         String confirmCountryName = countryEntity.getName();
         assertEquals(confirmCountryName, countryName);
+
         assertNotNull(countryEntity);
         assertNotNull(countryEntity.getCapital());
 
         Optional<CountryEntity> country = countryRepository.findById(countryCode);
         assertTrue(country.isPresent());
+
+        Assertions.assertEquals(countryEntity.getCapital(), cityRepository.findAll().getLast().getId());
+    }
+
+    @Test
+    @DisplayName("Create a new country doesnt create a new country with an existing country code ")
+    void createANewCountryDoesntCreateANewCountryWithAnExistingCountryCode() {
+        String countryCode = "USA";
+        String countryName = "TestCountry";
+        String continent = "Asia";
+        String region = "Southeast Asia";
+        BigDecimal surfaceArea = new BigDecimal("100000");
+        Short independenceYear = 2000;
+        Integer population = 1000000;
+        BigDecimal lifeExpectancy = new BigDecimal("75.5");
+        BigDecimal gnp = new BigDecimal("500000");
+        BigDecimal gnpOld = new BigDecimal("400000");
+        String localName = "TestCountry";
+        String governmentForm = "Republic";
+        String headOfState = "Test Leader";
+        String countryCode2 = "ZZ";
+        boolean hasACapital = false;
+
+        worldService.createNewCountry(countryCode, countryName, continent, region, surfaceArea, independenceYear,
+                population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, countryCode2,
+                hasACapital);
+
+        CountryEntity countryWhichWouldBeAdded = countryRepository.findAll().getLast();
+        String expected = "TestCountry";
+        Assertions.assertNotEquals(expected, countryWhichWouldBeAdded.getName());
+
+
+    }
+
+    @Test
+    @DisplayName("createNewCountryDoesNotAddNewCountryIfCode2Exists")
+    void createNewCountryDoesNotAddNewCountryIfCode2Exists() {
+        String countryCode = "ZZZ";
+        String countryName = "TestCountry";
+        String continent = "Asia";
+        String region = "Southeast Asia";
+        BigDecimal surfaceArea = new BigDecimal("100000");
+        Short independenceYear = 2000;
+        Integer population = 1000000;
+        BigDecimal lifeExpectancy = new BigDecimal("75.5");
+        BigDecimal gnp = new BigDecimal("500000");
+        BigDecimal gnpOld = new BigDecimal("400000");
+        String localName = "TestCountry";
+        String governmentForm = "Republic";
+        String headOfState = "Test Leader";
+        String countryCode2 = "US";
+        boolean hasACapital = false;
+
+        worldService.createNewCountry(countryCode, countryName, continent, region, surfaceArea, independenceYear,
+                population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, countryCode2,
+                hasACapital);
+
+        CountryEntity countryWhichWouldBeAdded = countryRepository.findAll().getLast();
+
+        String expected = "TestCountry";
+        Assertions.assertNotEquals(expected, countryWhichWouldBeAdded.getName());
+    }
+
+    @Test
+    @DisplayName("createNewCountryDoesntAcceptNegativePopulation")
+    void createNewCountryDoesntAcceptNegativePopulation() {
+        String countryCode = "ZZZ";
+        String countryName = "TestCountry";
+        String continent = "Asia";
+        String region = "Southeast Asia";
+        BigDecimal surfaceArea = new BigDecimal("100000");
+        Short independenceYear = 2000;
+        Integer population = -1;
+        BigDecimal lifeExpectancy = new BigDecimal("75.5");
+        BigDecimal gnp = new BigDecimal("500000");
+        BigDecimal gnpOld = new BigDecimal("400000");
+        String localName = "TestCountry";
+        String governmentForm = "Republic";
+        String headOfState = "Test Leader";
+        String countryCode2 = "ZZ";
+        boolean hasACapital = false;
+
+        worldService.createNewCountry(countryCode, countryName, continent, region, surfaceArea, independenceYear,
+                population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, countryCode2,
+                hasACapital);
+
+        CountryEntity countryWhichWouldBeAdded = countryRepository.findAll().getLast();
+
+        String expected = "TestCountry";
+        Assertions.assertNotEquals(expected, countryWhichWouldBeAdded.getName());
+    }
+
+    @Test
+    @DisplayName("Create new country language with valid parameters")
+    void createNewCountryLanguageWithValidParameters() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("50");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertEquals(expected, country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Doesnt create new country language for invalid country code")
+    void doesntCreateNewCountryLanguageForInvalidCountryCode() {
+        String countryCode = "ZZZ";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("50");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertNotEquals(expected, country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Doesnt create a new country language with zero percent spoken")
+    void doesntCreateANewCountryLanguageWithZeroPercentSpoken() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("0");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertNotEquals(expected, country.getLanguage(), country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Doesnt create a new country language with over 100 percent spoken")
+    void doesntCreateANewCountryLanguageWithOver100PercentSpoken() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("101");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertNotEquals(expected, country.getLanguage(), country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Creates new country language with minimum percent spoken")
+    void createsNewCountryLanguageWithMinimumPercentSpoken() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("0.1");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertEquals(expected, country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Creates new country language with 100 percent spoken")
+    void createsNewCountryLanguageWith100PercentSpoken() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "F";
+        BigDecimal percentageSpoken = new BigDecimal("100");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertEquals(expected, country.getLanguage());
+    }
+
+    @Test
+    @DisplayName("Doesnt create new country if isOfficial is not T or F")
+    void doesntCreateNewCountryIfIsOfficialIsNotTorF() {
+        String countryCode = "ZWE";
+        String countryLanguage = "TestCountryLanguage";
+        String isOfficial = "X";
+        BigDecimal percentageSpoken = new BigDecimal("50");
+
+        worldService.createNewCountryLanguage(countryCode, countryLanguage, isOfficial, percentageSpoken);
+
+        CountryLanguageEntity country = countryLanguageRepository.findAll().getLast();
+        String expected = "TestCountryLanguage";
+        Assertions.assertNotEquals(expected, country.getLanguage());
     }
 
 }
