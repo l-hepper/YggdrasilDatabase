c00dfc345b49d2f34795fb54bb78ca6f4bf15550
Devin Colyer <dcolyer@spartaglobal.com>
2024-07-17 11:59:19 +0100

    Added exception handler

---
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.mjolnir.yggdrasil.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
+        ErrorResponse err = new ErrorResponse("NOT_FOUND", e.getMessage(), request.getRequestURL().toString());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("NOT_FOUND", e.getMessage(), request.getRequestURL().toString()));
+    }
+
+    @ExceptionHandler(IdMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleIdMismatchException(IdMismatchException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("BAD_REQUEST", e.getMessage(), request.getRequestURL().toString()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(
+                error -> errors.put(((FieldError)error).getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("BAD_REQUEST", errors, request.getRequestURL().toString()));
+    }
+
+    private record ErrorResponse<T>(String error, T errorInfo, String url) {}
+}
---
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/exceptions/IdMismatchException.java
@@ -0,0 +1,7 @@
+package com.mjolnir.yggdrasil.exceptions;
+
+public class IdMismatchException extends RuntimeException {
+    public IdMismatchException(String msg) {
+        super(msg);
+    }
+}
