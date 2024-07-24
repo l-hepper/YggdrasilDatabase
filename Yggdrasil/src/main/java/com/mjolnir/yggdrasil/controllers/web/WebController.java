a8e472e217eca82da19255f03761148d9b0787c9
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-24 21:30:55 +0100

    feat: Add controller to handle login and home page

---
+++ Yggdrasil/src/main/java/com/mjolnir/yggdrasil/controllers/web/WebController.java
@@ -0,0 +1,24 @@
+package com.mjolnir.yggdrasil.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDate;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/Yggdrasil")
+    public String index(Model model) {
+        model.addAttribute("date", LocalDate.now());
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+}
